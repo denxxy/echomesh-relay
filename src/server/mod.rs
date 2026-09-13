aca6593c1b8e5d920ff301d7d55737adf750161b
denxxy <113539027+denxxy@users.noreply.github.com>
2026-09-13 09:55:34 +0300

    refactor(relay): enable routed listener

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,3 +1,5 @@
 pub mod listener;
+pub mod routed_listener;
 
-pub use listener::{ListenerConfig, PrefixedStream, RelayListener, RelaySecrets};
+pub use listener::{ListenerConfig, RelaySecrets};
+pub use routed_listener::{PrefixedStream, RelayListener, ROUTE_REGISTRATION_ID};
